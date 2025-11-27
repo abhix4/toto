49ae3b8cee31f165b09f09d81ebeae8326c2420f
abhi <abhiifour@gmail.com>
2025-11-27 20:42:03 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-   <div>
-
+   <div className="text-yellow-800 flex justify-center items-center h-screen">
+    <h2 className="text-8xl">Todos</h2>
    </div>
   );
 }
